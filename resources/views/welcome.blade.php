eb4f1ce96b73604bd2cba4d2a1e0ff33e6eecd1c
= <=>
2019-12-21 12:25:35 +0530

    update homapase for new feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    welcome too Laravel
                 </div>
 
                 <div class="links">
